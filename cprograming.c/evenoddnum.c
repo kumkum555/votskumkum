97daf24a8e11f0da33c9238db670c3c0224c9991
Kumkum555 <Kumkumkapkoti@gmail.com>
2024-07-03 14:40:39 +0530

    evennumberoddnumber

---
+++ cprograming.c/evenoddnum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+
+int number;
+
+printf("please enter any number :");
+scanf("%d",&number); 
+
+if (number % 2 ==0)
+{
+    printf("%d is even.\n",number);
+}
+
+else {
+
+   printf("%d is odd.\n",number); 
+}
+
+
+    return 0;
+}
